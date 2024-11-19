351110a2db34c73186dd6544018993c483f25607
Samuel1-salc <andrade.carneiro@mail.uft.edu.br>
2024-11-19 19:35:36 -0300

    real commit antes de dar deploy rs

--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const path = require('path');
-//const chatbot = require('./chatbot'); 
+const chatbot = require('./chatbot'); 
 const app = express();
+const bodyParser = require('body-parser'); // Add body-parser
 
 const port = 3000;
 
-
+// Use body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'frontend')));
 // Rota do chatbot
 chatbot.start();
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname,'frontend',  'login'));
-  
+  res.sendFile(path.join(__dirname,'frontend',  'login.html')); // Use sendFile instead of render
 });
-
 
 // Inicia o servidor
 app.listen(port, () => {
@@ -23,4 +24,3 @@ app.listen(port, () => {
 });
 
 
-express.Router
