244c0fe44204dede5bc3c06b4fc61465db9b1d6d
Samuel1-salc <andrade.carneiro@mail.uft.edu.br>
2024-11-28 01:20:11 -0300

    sem porta

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const chatbot = require('./chatbot'); 
 const app = express();
 
-const primaryPort = process.env.PORT || 3000; // Porta principal
-const secondaryPort = 4000; // Porta alternativa
+//const port = process.env.PORT || 3000; // Use the PORT environment variable if available
 
 // Use Express built-in middleware  
 app.use(express.urlencoded({ extended: true }));
@@ -18,23 +17,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'frontend',  'login.html')); // Use sendFile instead of render
 });
 
-// Função para iniciar o servidor
-const startServer = (port) => {
-  app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  }).on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
-      console.log(`Porta ${port} já está em uso, tentando porta ${secondaryPort}`);
-      if (port === primaryPort) {
-        startServer(secondaryPort);
-      } else {
-        console.error('Ambas as portas estão em uso. Não foi possível iniciar o servidor.');
-      }
-    } else {
-      console.error('Erro ao iniciar o servidor:', err);
-    }
-  });
-};
-
-// Inicia o servidor na porta principal
-startServer(primaryPort);
+// Inicia o servidor
+/*app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});*/
