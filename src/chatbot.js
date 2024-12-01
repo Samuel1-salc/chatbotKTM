f9214b680e01cf12e047742ad4fdc104601f85ca
Samuel1-salc <andrade.carneiro@mail.uft.edu.br>
2024-12-01 01:00:57 -0300

    deus me ama?

--- src/chatbot.js
+++ src/chatbot.js
@@ -42,20 +42,16 @@ function start() {
     console.log('iniciando');
     clearStates(); // Clear states at the start
     const qrcode = require('qrcode');
-    const { Client, Buttons, List, MessageMedia,LocalAuth } = require('whatsapp-web.js'); // Mudança Buttons
+    const { Client, Buttons, List, MessageMedia, } = require('whatsapp-web.js'); // Mudança Buttons
     
   
     let contactStates = loadStates(); // Load states from the JSON file
     console.log('iniciando2');
 
-    const client = new Client({
-        authStrategy: new LocalAuth(),
-        // proxyAuthentication: { username: 'username', password: 'password' },
-        puppeteer: { 
-            // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
-            headless: false,
-        }
-    });
+    const client = new Client;
+       
+    
+      
     
     // client initialize does not finish at ready now.
     client.initialize();
